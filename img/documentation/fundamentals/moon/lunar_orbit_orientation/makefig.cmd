51e042220d1f37df41191eff4437f5d7f0dbb74c
smolkaa <alex@csmolka.de>
2023-07-12 07:38:44 +0200

    update

---
+++ img/documentation/fundamentals/moon/lunar_orbit_orientation/makefig.cmd
@@ -0,0 +1,9 @@
+latex lunar_orbit_orientation.tex
+dvisvgm lunar_orbit_orientation.dvi
+
+DEL *.aux
+DEL *.dvi
+DEL *.fdb_latexmk
+DEL *.fls
+DEL *.log
+DEL *.xdv
---
+++ img/documentation/fundamentals/moon/lunar_orbit_orientation/makefig.sh
@@ -0,0 +1,9 @@
+latex lunar_orbit_orientation.tex
+dvisvgm lunar_orbit_orientation.dvi
+
+rm *.aux
+rm *.dvi
+rm *.fdb_latexmk
+rm *.fls
+rm *.log
+rm *.xdv
