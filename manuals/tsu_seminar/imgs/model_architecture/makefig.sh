e49cd06a3810bdeb94442e0649cd0bbe291e5aa3
smolkaa <alex@csmolka.de>
2023-08-07 14:38:11 +0200

    edited TSU presentation page

--- manuals/tsu_seminar/imgs/model_architecture/makefig.sh
+++ manuals/tsu_seminar/imgs/model_architecture/makefig.sh
@@ -2,10 +2,25 @@
 #latex model_architecture.tex
 #dvisvgm model_architecture.dvi
 
-pdflatex loop_I.tex
-pdftocairo -svg loop_I.pdf
-pdftocairo -png -r 600 loop_I.pdf
+#pdflatex loop_I.tex
+#pdftocairo -svg loop_I.pdf
+#pdftocairo -png -r 600 loop_I.pdf
+#mv loop_I-1.png loop_I.png
 
+#pdflatex loop_II.tex
+#pdftocairo -svg loop_II.pdf
+#pdftocairo -png -r 600 loop_II.pdf
+#mv loop_II-1.png loop_II.png
+
+#pdflatex loop_III.tex
+#pdftocairo -svg loop_III.pdf
+#pdftocairo -png -r 600 loop_III.pdf
+#mv loop_III-1.png loop_III.png
+
+pdflatex loop_IV.tex
+pdftocairo -svg loop_IV.pdf
+pdftocairo -png -r 600 loop_IV.pdf
+mv loop_IV-1.png loop_IV.png
 
 rm *.aux
 rm *.dvi
