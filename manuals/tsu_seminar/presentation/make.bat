4e8e262682ae287c84a07033ddc7e2179fc98d28
Alexander Smolka <alexander.smolka@tum.de>
2023-08-10 16:11:46 +0200

    added quarto revealjs presentation for TSU, integrated into manual page

---
+++ manuals/tsu_seminar/presentation/make.bat
@@ -0,0 +1,6 @@
+quarto render _presentation.qmd
+
+robocopy "_presentation_files/" "../../../docs/manuals/tsu_seminar/presentation/_presentation_files/" /e
+copy "tum.png" "../../../docs/manuals/tsu_seminar/presentation/"
+copy "_style.css" "../../../docs/manuals/tsu_seminar/presentation/"
+copy "_presentation.html" "../../../docs/manuals/tsu_seminar/presentation/"
---
+++ manuals/tsu_seminar/presentation/make.sh
@@ -0,0 +1,6 @@
+quarto render _presentation.qmd
+
+cp _presentation_files/ ../../../docs/manuals/tsu_seminar/presentation/
+cp tum.png ../../../docs/manuals/tsu_seminar/presentation/
+cp _style.css ../../../docs/manuals/tsu_seminar/presentation/
+cp _presentation.html ../../../docs/manuals/tsu_seminar/presentation/
