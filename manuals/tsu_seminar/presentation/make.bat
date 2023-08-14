0b36a90dd2a6b25bde7eb27a7fc547fca7913347
Alexander Smolka <alexander.smolka@tum.de>
2023-08-14 14:23:28 +0200

    first TSU presentation version upload

--- manuals/tsu_seminar/presentation/make.bat
+++ manuals/tsu_seminar/presentation/make.bat
@@ -4,3 +4,5 @@ robocopy "_presentation_files/" "../../../docs/manuals/tsu_seminar/presentation/
 copy "tum.png" "../../../docs/manuals/tsu_seminar/presentation/"
 copy "_style.css" "../../../docs/manuals/tsu_seminar/presentation/"
 copy "_presentation.html" "../../../docs/manuals/tsu_seminar/presentation/"
+
+robocopy "../imgs" "../../../docs/manuals/tsu_seminar/imgs" /e
