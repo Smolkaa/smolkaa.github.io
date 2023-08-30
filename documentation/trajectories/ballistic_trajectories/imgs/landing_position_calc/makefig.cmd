fa95751be05108cd54b5305a04b4a729c57c3b1a
Alexander Smolka <alexander.smolka@tum.de>
2023-08-30 16:19:06 +0200

    update landing position calculation with image

--- documentation/trajectories/ballistic_trajectories/imgs/landing_position_calc/makefig.cmd
+++ documentation/trajectories/ballistic_trajectories/imgs/landing_position_calc/makefig.cmd
@@ -1,5 +1,7 @@
-latex landing_position_calc.tex
-dvisvgm landing_position_calc.dvi
+pdflatex landing_position_calc.tex
+pdftocairo -svg landing_position_calc.pdf
+pdftocairo -png -r 600 landing_position_calc.pdf
+move landing_position_calc-1.png landing_position_calc.png
 
 DEL *.aux
 DEL *.dvi
