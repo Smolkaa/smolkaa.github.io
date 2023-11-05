7be72175051324a18304d0d821740f2fc92660ca
Alexander Smolka <a.smolka@tum.de>
2023-11-05 22:48:03 +0100

    updated distirbutions api + some distribution documentation

--- resources/julia/colors.jl
+++ resources/julia/colors.jl
@@ -8,7 +8,3 @@ TUMBlueDarker = colorant"#003359"
 TUMBlueLight = colorant"#64A0C8"
 TUMBlueLighter = colorant"#98C6EA"
 TUMOrange = colorant"#E37222"
-
-
-#::. custom ColorSchemes
-# include(joinpath(@__DIR__, "lipari.jl"))
